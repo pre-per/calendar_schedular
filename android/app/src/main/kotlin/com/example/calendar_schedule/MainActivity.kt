51788e62f25beba2e644a9d4d2a5fadecabc802f
pre-per <snowha9557@korea.ac.kr>
2024-10-27 16:29:00 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/calendar_schedule/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.calendar_schedule
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Hello World"),
+    );
+  }
+  
+}
