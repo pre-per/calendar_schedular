778332e72b6dbb0a3eaf65c3178ea38f8566bda4
pre-per <snowha9557@korea.ac.kr>
2024-10-27 18:26:48 +0900

    today_banner done

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:calendar_schedule/component/main_calendar.dart';
+import 'package:calendar_schedule/component/schedule_card.dart';
+import 'package:calendar_schedule/component/today_banner.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -24,6 +26,17 @@ class _HomeScreenState extends State<HomeScreen> {
             MainCalendar(
               selectedDate: selectedDate,
               onDaySelected: onDaySelected,
+            ),
+            const SizedBox(height: 8.0),
+            TodayBanner(
+              selectedDate: selectedDate,
+              count: 0,
+            ),
+            const SizedBox(height: 8.0),
+            const ScheduleCard(
+              startTime: 12,
+              endTime: 14,
+              content: '프로그래밍 공부'
             ),
           ],
         ),
