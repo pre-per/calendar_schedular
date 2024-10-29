a559ee4817e57fc8eb9c7ab18e06e9ebda5241f0
pre-per <snowha9557@korea.ac.kr>
2024-10-29 13:57:27 +0900

    bottomsheet done

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -13,12 +13,15 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
     return SafeArea(
       child: Container(
-        height: MediaQuery.of(context).size.height / 2,
+        height: MediaQuery.of(context).size.height / 2 + bottomInset,
         color: Colors.white,
         child: Padding(
-          padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+          padding: EdgeInsets.only(left: 8, right: 8, top: 8,
+              bottom: bottomInset),
           child: Column(
             children: [
               Row(
