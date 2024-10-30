e3215b5d9844a9a49737d6276616dae40b63cbac
pre-per <snowha9557@korea.ac.kr>
2024-10-30 10:54:23 +0900

    drift_sql

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -10,66 +10,120 @@ class ScheduleBottomSheet extends StatefulWidget {
 }
 
 class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  int? startTime;
+  int? endTime;
+  String? content;
 
   @override
   Widget build(BuildContext context) {
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
 
-    return SafeArea(
-      child: Container(
-        height: MediaQuery.of(context).size.height / 2 + bottomInset,
-        color: Colors.white,
-        child: Padding(
-          padding: EdgeInsets.only(left: 8, right: 8, top: 8,
-              bottom: bottomInset),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: CustomTextField(
-                      label: '시작 시간',
-                      isTime: true,
+    return Form(
+      key: formKey,
+      child: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(left: 8, right: 8, top: 8,
+                bottom: bottomInset),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        label: '시작 시간',
+                        isTime: true,
+                        onSaved: (String? val) {
+                          startTime = int.parse(val!);
+                        },
+                        validator: timeValidator,
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
-                    child: CustomTextField(
-                      label: '종료 시간',
-                      isTime: true,
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: CustomTextField(
+                        label: '종료 시간',
+                        isTime: true,
+                        onSaved: (String? val) {
+                          endTime = int.parse(val!);
+                        },
+                        validator: timeValidator,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 8.0),
-              Expanded(
-                child: CustomTextField(
-                  label: '내용',
-                  isTime: false,
+                  ],
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: onSavePressed,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: PRIMARY_COLOR,
+                const SizedBox(height: 8.0),
+                Expanded(
+                  child: CustomTextField(
+                    label: '내용',
+                    isTime: false,
+                    onSaved: (String? val) {
+                      content = val;
+                    },
+                    validator: contentValidator,
                   ),
-                  child: const Text(
-                    '저장',
-                    style: TextStyle(
-                      color: Colors.white,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: onSavePressed,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: PRIMARY_COLOR,
+                    ),
+                    child: const Text(
+                      '저장',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
   void onSavePressed() {
+    if (formKey.currentState!.validate()){
+      formKey.currentState!.save();
 
+      print(startTime);
+      print(endTime);
+      print(content);
+    }
+  }
+
+  String? timeValidator(String? val) {
+    if (val == null) {
+      return '값을 입력해주세요';
+    }
+
+    int? number;
+
+    try {
+      number = int.parse(val);
+    } catch (e) {
+      return '숫자를 입력해주세요';
+    }
+
+    if (number < 0 || number > 24) {
+      return '0시부터 24시 사이를 입력해주세요';
+    }
+
+    return null;
+  }
+
+  String? contentValidator(String? val) {
+    if (val == null || val.length == 0) {
+      return '값을 입력해주세요';
+    }
+
+    return null;
   }
 }
